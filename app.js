1f85c8741a1d21c7aa6af80577b45a45ab9c2219
Iain Watt <8229186-iainwatt@users.noreply.gitlab.com>
2023-03-20 12:50:35 +0000

    split by line

--- app.js
+++ app.js
@@ -6,7 +6,11 @@ var fs = require('fs');
 
 try {
     var data = fs.readFileSync('MovieScripts/dances_with_wolves.txt', 'utf8');
-    console.log(data.toString());
 } catch (e) {
     console.log('Error:', e.stack);
 }
+
+
+data.split(/\r?\n/).forEach(line => {
+    console.log(`--> ${line}`);
+});
